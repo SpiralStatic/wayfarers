be4a466dda8303cecd4ce50eb80080ea1eff3e98
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-15 20:57:03 +0000

    Added schema to location model

--- src/models/location.js
+++ src/models/location.js
@@ -2,7 +2,31 @@ var mongoose = require('mongoose');
 
 // Create a new schema
 var locationSchema = mongoose.Schema({
-    location: String
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        maxlength: 500
+    },
+    Longitude: {
+        type: Number,
+        min: -180,
+        max: 180
+    },
+    Latitude: {
+        type: Number,
+        min: -90,
+        max: 90
+    },
+    images: [{
+        type: String
+    }]
 });
 
 // Tell mongoose to create a real model from our schema and export it
