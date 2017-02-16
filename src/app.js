036cf7bd3722dfa79a453a37fdabe55f26a55fcf
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-16 20:41:25 +0000

    Added cookie, sesssion and flash message functionality

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ var layouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
+//var cookieParser = require('cookie-parser');
+//var session = require('express-session');
+//var flash = require('connect-flash');
 
 var routes = require('./config/routes');
 
@@ -12,6 +15,27 @@ var app = express();
 mongoose.connect('mongodb://localhost/wayfarers', function() {
     console.log("Database is now connected");
 });
+
+// Add support for cookies
+//app.use(cookieParser());
+
+// Add support for sessions
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: true,
+//     secret: 'wayfarersuniteundertheyellowsun'
+// }));
+
+// Add flash messaging support
+// app.use(flash());
+
+// Allow flash messages to be used in every template
+// app.use(function(req, res, next){
+//     // res.locals will be available in every template
+//     res.locals.errors = req.flash('error');
+//     console.log(res.locals.errors);
+//     next();
+// });
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,6 +55,15 @@ app.set('view engine', 'ejs');
 
 // Include express layouts middleware
 app.use(layouts);
+
+// Check for user login
+// app.use(function(req, res, next) {
+//     var urls = ["/sessions/new", "/users/new", "/sessions", "/users"];
+//     if(urls.indexOf(req.url) === -1) {
+//         if (!req.user) return res.redirect('/sessions/new');
+//     }
+//     next();
+// });
 
 // Add the router
 app.use(routes);
